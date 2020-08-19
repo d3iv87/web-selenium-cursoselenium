280afdcf2b65ab789a9f2040f064a296c5d1a838
d3iv87 <69394188+d3iv87@users.noreply.github.com>
2020-08-18 20:34:05 -0500

    commit

---
+++ SELENIUM/src/test/java/com/javabasic/Datatypes.java
@@ -0,0 +1,46 @@
+package com.javabasic;
+
+public class Datatypes {
+
+	public Datatypes() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*Datos numericos*/
+		//		byte x ;
+//		short y;
+//		int z;
+//		long a;
+//		
+//		x=127;
+//		y=32767;
+//		z=2147483647;
+//		a=9223372036854775807;
+//	
+//		System.out.println(x);
+
+/*Datos numericos reales*/
+//		float base;
+//		double altura;
+//		base=30.5F;   //  se 
+//		altura=40.76;
+//		
+//		System.out.println(base);
+		
+		/*Datos caracter y logico*/
+		char letra1;
+		char letra2;
+		String frase;
+		
+		//letra1='h';
+		//letra2='Niño';
+		//frase='hola';
+		
+		//System.out.println(frase);
+		
+
+	}
+
+}
---
+++ SELENIUM/src/test/java/com/javabasic/OperatorsStatements.java
@@ -0,0 +1,42 @@
+package com.javabasic;
+
+public class OperatorsStatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*Operadores aritmeticos*/
+//		int X=1;
+//		int y=2;
+//		int suma=X+y;
+//		
+//		System.out.println(suma);
+		
+		/*
+		 Operadores unarios
+		 */
+//		int valor1=3;
+//		int incrementar = --valor1;
+//		
+//		System.out.println(incrementar);
+
+		/*
+		 operadores relacionales y condicionales
+		 */
+		int num1=10;
+		int num2=4;
+		int num3=6;
+		int nnum4=11;
+		if (num1>=10 && num2>10) {
+			
+			System.out.println("Los numeros son mayores a 10");
+		}
+		else {
+			System.out.println("Los numeros son menores");
+			
+		}
+		
+		
+	}
+
+}
